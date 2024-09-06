9627753c048e90ecb4e42400e09ab4f9796ce86b
youginongjoon <hjoon990@gmail.com>
2024-09-07 03:55:17 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Customer from "./components/Customer";
 
+const customers = [
+  {
+    id: 1,
+    image: "https://picsum.photos/id/1/64/64",
+    name: "홍길동",
+    birthday: "990909",
+    gender: "여자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/id/2/64/64",
+    name: "김학수",
+    birthday: "971203",
+    gender: "남자",
+    job: "교사",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/id/3/64/64",
+    name: "이순신",
+    birthday: "930921",
+    gender: "여자",
+    job: "군인",
+  },
+];
 
 class App extends Component {
   render() {
     return (
-      <div className='gray-background'>
-        <img src={logo} lat="logo"/>
-        <h2>Let's develop management system</h2>
-      </div>
-    )
+      <>
+        {customers.map((c) => {
+          return (
+            <Customer
+              key={c.id}
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })}
+      </>
+    );
   }
 }
-
 
 export default App;
