c21171a3aec4a7866d4c237998b7538e462dd9b2
youginongjoon <hjoon990@gmail.com>
2024-09-13 21:32:56 +0900

    Updata UI/UX & Add Customer Search Module

--- client/src/App.js
+++ client/src/App.js
@@ -9,13 +9,20 @@ import TableRow from "@mui/material/TableRow";
 import Customer from "./components/Customer";
 import Paper from "@mui/material/Paper";
 import CircularProgress from "@mui/material/CircularProgress";
+import SearchAppBar from "./components/SearchBar";
 
 function App() {
   const [customers, setCustomers] = useState([]);
   const [progress, setProgress] = useState(0);
+  const [searchState, setSearchState] = useState('');
+
+  const handleSearchValueChange = (e) => {
+    setSearchState(e.target.value);
+  }
 
   const stateRefresh = () => {
     setCustomers([]);
+    setSearchState('');
     callApi()
       .then((res) => setCustomers(res))
       .catch((err) => console.log(err));
@@ -45,36 +52,43 @@ function App() {
     };
   }, []);
 
+  const cellList = [
+    "번호",
+    "이미지",
+    "이름",
+    "생년월일",
+    "성별",
+    "직업",
+    "설정",
+  ];
+
+  const filteredComponents = (data) => {
+    data = data.filter((c) => {
+      return c.name.indexOf(searchState) > -1;
+    })
+    return data.map((c) => {
+      return <Customer stateRefresh={stateRefresh} key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>
+    })
+  }
+
   return (
     <>
-      <Paper className="root">
+      <SearchAppBar handleSearchValueChange={handleSearchValueChange} searchState={searchState}/>
+      <div className="menu">
+        <CustomerAdd stateRefresh={stateRefresh} />
+      </div>
+
+      <Paper className="paper">
         <Table className="table">
           <TableHead>
             <TableRow>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              {cellList.map((c, index) => {
+                return <TableCell key={index} className="tableHead">{c}</TableCell>
+              })}
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.length !== 0 ? (
-              customers.map((c) => (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                  stateRefresh={stateRefresh}
-                />
-              ))
-            ) : (
+            {customers.length !== 0 ? filteredComponents(customers) : (
               <TableRow>
                 <TableCell colSpan="6" align="center">
                   <CircularProgress
@@ -88,7 +102,6 @@ function App() {
           </TableBody>
         </Table>
       </Paper>
-      <CustomerAdd stateRefresh={stateRefresh} />
     </>
   );
 }
